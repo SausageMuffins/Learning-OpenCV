2819de05c44ee5b8880eb8334f04b7abba32b90d
SausageMuffins <rwang9098@gmail.com>
2023-05-06 18:34:35 +0800

    Placing Items on Images using OpenCV

--- SeePuppy.py
+++ SeePuppy.py
@@ -6,7 +6,9 @@ img = cv2.imread("C://Users//nryan//OneDrive//Desktop//00-puppy.jpg")
 while True:
     cv2.imshow("Dog", img) # show the image using cv2's library.
     
-    # Check if we have waited 1ms and we press the escape button which has encoding 0xFF.
+    # Check if we have waited 1ms and we press the ESC button which has encoding 0xFF.
     
     if cv2.waitKey()(1) & 0xFF == 27: # & is a bitwise comparison.
         break
+    
+cv2.destroyAllWindows() 
